c73131fedbc3456929cf615a72ac486ee87ecdb5
Niels Gregers Johansen <niels@timeboxer.dk>
2024-11-02 12:16:17 +0300

    added insights

--- .koksmat/web/app/layout.tsx
+++ .koksmat/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Script from "next/script";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { CLARITY } from "./global";
 import RootLayoutClientSide from "./layout-client";
 
@@ -13,15 +13,7 @@ export default function RootLayout2({
   return (
     <html lang="en">
       <body>
-        <Script id="clarityinjection">
-          {`
-    (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "${CLARITY}");            
-            `}
-        </Script>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -30,6 +22,8 @@ export default function RootLayout2({
         ><RootLayoutClientSide>
 
             {children}</RootLayoutClientSide>
+          <SpeedInsights />
+
         </ThemeProvider>
       </body>
     </html>
